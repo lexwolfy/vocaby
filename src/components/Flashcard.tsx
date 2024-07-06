8840c3d33ca8a835dbe50658d3e58ec10c64a91d
lexwolfy <alexis.ageorges@gmail.com>
2024-07-06 16:18:33 +0800

    feat: Add text to speech

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from 'antd';
-import { SoundOutlined } from '@ant-design/icons';
+import {SoundOutlined, SwapOutlined} from '@ant-design/icons';
 import { useThemeContext } from '../ThemeContext';
 import fr from "../assets/fr.svg";
 import en from "../assets/uk.svg";
@@ -23,15 +23,21 @@ interface FlashcardProps {
     flipped: boolean;
     onFlip: () => void;
 }
+const synth = window.speechSynthesis;
 
 const Flashcard: React.FC<FlashcardProps> = ({ word, category, subcategory, flipped, onFlip }) => {
     const { language } = useThemeContext();
     const lang = flipped ? (language === 'fr' ? 'english' : 'french') : (language === 'fr' ? 'french' : 'english');
     const flagSrc = flipped ? (language === 'fr' ? en : fr) : (language === 'fr' ? fr : en);
 
+    const onSpeak = () => {
+        const utterance = new SpeechSynthesisUtterance(word[lang]);
+        utterance.lang = lang === 'french' ? 'fr-FR' : 'en-US';
+        synth.speak(utterance);
+    }
+
     return (
         <Card
-            onClick={onFlip}
             style={{
                 width: 300,
                 textAlign: 'center',
@@ -39,15 +45,16 @@ const Flashcard: React.FC<FlashcardProps> = ({ word, category, subcategory, flip
                 position: 'relative',
             }}
             actions={[
-                <SoundOutlined key="sound" />
+                <SwapOutlined key="swap" onClick={onFlip} />,
+                <SoundOutlined key="sound" onClick={onSpeak} />
             ]}
         >
             <img src={flagSrc} alt="flag" style={{ position: 'absolute', top: 10, left: 10, width: 24 }} />
-            <div style={{ marginTop: 50 }}>{word[lang]}</div>
-            <div style={{ marginTop: 20, fontSize: '0.8em', color: 'gray' }}>
+            <div style={{ marginTop: 50, fontSize: '1.5em' }}>{word[lang]}</div>
+            <div style={{ marginTop: 20, color: 'gray' }}>
                 {category[lang]}
             </div>
-            <div style={{ fontSize: '0.8em', color: 'gray' }}>
+            <div style={{ color: 'gray' }}>
                 {subcategory[lang]}
             </div>
         </Card>
